0898e7844128108e9fb68774ec9c23e7460e32be
Sudheer <sudhirsuri43@gmail.com>
2020-03-15 17:58:07 -0400

    added comments

--- javascript/questionscript.js
+++ javascript/questionscript.js
@@ -1,12 +1,14 @@
+/*answers() function applies light green background to all the elements with id = answer */
 function answers()
 {
    var i=0;
-   
-   rightanswer = document.querySelectorAll("#answer");
-   
-   while(i<rightanswer.length)
+   /*select all elements with id answer using querySelectorAll method and load into variable called rightanswers */
+   rightanswers = document.querySelectorAll("#answer");
+  
+   /*loop through each right answer and apply light green background*/
+   while(i<rightanswers.length)
    {
-   	 rightanswer[i].style.background="#ADFF2F";  
+   	 rightanswers[i].style.background="#ADFF2F";  
      i++;
    }
 }
